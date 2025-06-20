4b055e043b0382ed1a7e556f31ae2faa0ed4267a
Wataru2936 <wataru2936@gmail.com>
2025-06-20 13:36:27 +0900

    Add GitHub Pages deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/maeda-takuho-anniversary' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/maeda-takuho-anniversary' : '',
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/maeda57/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/maeda57' : '',
 };
 
 export default nextConfig;
